7ab81b5e3f0107a80374088dcd7f6b4a6b3ac6ce
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2019-05-20 17:21:19 +0200

    Add template for factory benchmarks

---
+++ src/main/java/de/hhu/bsinfo/neutrino/util/ResultFactory.java
@@ -0,0 +1,10 @@
+package de.hhu.bsinfo.neutrino.util;
+
+import de.hhu.bsinfo.neutrino.struct.Result;
+
+public class ResultFactory {
+
+    public Result newInstance() {
+        return new Result();
+    }
+}
