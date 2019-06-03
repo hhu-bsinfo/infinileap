d4f90a6402a9a0408e8423e83d08b42f46e1574d
Filip Krakowski <Filip.Krakowski@Uni-Duesseldorf.de>
2019-06-03 16:58:29 +0200

    Implement absolute bulk read/write with offset and length within RemoteBuffer

--- src/main/java/de/hhu/bsinfo/neutrino/buffer/RegisteredBuffer.java
+++ src/main/java/de/hhu/bsinfo/neutrino/buffer/RegisteredBuffer.java
@@ -1,6 +1,7 @@
 package de.hhu.bsinfo.neutrino.buffer;
 
 import de.hhu.bsinfo.neutrino.verbs.MemoryRegion;
+import de.hhu.bsinfo.neutrino.verbs.ScatterGatherElement;
 
 public class RegisteredBuffer extends LocalBuffer implements AutoCloseable {
 
@@ -29,6 +30,27 @@ public class RegisteredBuffer extends LocalBuffer implements AutoCloseable {
 
     public void write(RemoteBuffer remoteBuffer) {
         remoteBuffer.write(this);
+    }
+
+    public ScatterGatherElement.Array split() {
+        return split(0, capacity());
+    }
+
+    public ScatterGatherElement.Array split(final long offset, final long length) {
+        long capacity = Math.abs(length - offset);
+        if (capacity <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int slots = (int) (capacity / Integer.MAX_VALUE) + 1;
+        int remainder = (int) (capacity & Integer.MAX_VALUE);
+
+        var array = new ScatterGatherElement.Array(slots + 1);
+        return array.forEachIndexed((index, element) -> {
+            element.setLocalKey(getLocalKey());
+            element.setLength(index == slots ? Integer.MAX_VALUE : remainder);
+            element.setAddress(getHandle() + offset + (long) index * Integer.MAX_VALUE);
+        });
     }
 
     @Override
--- src/main/java/de/hhu/bsinfo/neutrino/buffer/RemoteBuffer.java
+++ src/main/java/de/hhu/bsinfo/neutrino/buffer/RemoteBuffer.java
@@ -1,6 +1,5 @@
 package de.hhu.bsinfo.neutrino.buffer;
 
-import de.hhu.bsinfo.neutrino.util.BufferUtil;
 import de.hhu.bsinfo.neutrino.verbs.QueuePair;
 import de.hhu.bsinfo.neutrino.verbs.SendWorkRequest;
 import de.hhu.bsinfo.neutrino.verbs.SendWorkRequest.OpCode;
@@ -12,11 +11,13 @@ public class RemoteBuffer {
 
     private final QueuePair queuePair;
     private final long address;
+    private final long capacity;
     private final int key;
 
-    public RemoteBuffer(QueuePair queuePair, long address, int key) {
+    public RemoteBuffer(QueuePair queuePair, long address, long capacity, int key) {
         this.queuePair = queuePair;
         this.address = address;
+        this.capacity = capacity;
         this.key = key;
     }
 
@@ -24,16 +25,28 @@ public class RemoteBuffer {
         execute(OpCode.RDMA_READ, localBuffer);
     }
 
+    public void read(long index, RegisteredBuffer buffer, long offset, long length) {
+        execute(OpCode.RDMA_READ, index, buffer, offset, length);
+    }
+
     public void write(RegisteredBuffer localBuffer) {
         execute(OpCode.RDMA_WRITE, localBuffer);
     }
 
+    public void write(long index, RegisteredBuffer buffer, long offset, long length) {
+        execute(OpCode.RDMA_WRITE, index, buffer, offset, length);
+    }
+
     private void execute(final OpCode operation, RegisteredBuffer buffer) {
-        var elements = BufferUtil.split(buffer);
+        execute(operation, 0, buffer, 0, buffer.capacity());
+    }
+
+    private void execute(final OpCode operation, long index, RegisteredBuffer buffer, long offset, long length) {
+        var elements = buffer.split(offset, length);
 
         var request = new SendWorkRequest(config -> {
             config.setOpCode(operation);
-            config.rdma.setRemoteAddress(address);
+            config.rdma.setRemoteAddress(address + index);
             config.rdma.setRemoteKey(key);
             config.setFlags(SendFlag.SIGNALED);
             config.setListHandle(elements.getHandle());
