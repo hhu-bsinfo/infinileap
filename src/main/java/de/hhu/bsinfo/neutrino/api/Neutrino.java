f5d3ac677439ed8dcba76d9653146ce4ed43d20a
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2019-07-01 14:58:18 +0200

    Generate build configuration

--- src/main/java/de/hhu/bsinfo/neutrino/api/Neutrino.java
+++ src/main/java/de/hhu/bsinfo/neutrino/api/Neutrino.java
@@ -1,5 +1,26 @@
 package de.hhu.bsinfo.neutrino.api;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+import de.hhu.bsinfo.neutrino.generated.BuildConfig;
+
 public class Neutrino {
 
+    public static void printBanner() {
+        InputStream inputStream = Neutrino.class.getClassLoader().getResourceAsStream("banner.txt");
+
+        if (inputStream == null) {
+            return;
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String banner = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+
+        System.out.print("\n");
+        System.out.printf(banner, BuildConfig.VERSION, BuildConfig.BUILD_DATE, BuildConfig.GIT_BRANCH, BuildConfig.GIT_COMMIT);
+        System.out.print("\n\n");
+    }
 }
