9a4c2389734084413e5ffd795f357af2e5534f66
Filip Krakowski <krakowski@hhu.de>
2020-08-03 16:24:11 +0200

    Make Struct class implement MemorySegment

--- src/main/java/de/hhu/bsinfo/neutrino/verbs/panama/util/Struct.java
+++ src/main/java/de/hhu/bsinfo/neutrino/verbs/panama/util/Struct.java
@@ -4,9 +4,10 @@ import jdk.incubator.foreign.MemoryAddress;
 import jdk.incubator.foreign.MemoryLayout;
 import jdk.incubator.foreign.MemorySegment;
 
+import java.nio.ByteBuffer;
 import java.util.function.Supplier;
 
-public class Struct implements NativeObject {
+public class Struct implements MemorySegment {
 
     /**
      * This struct's backing memory segment.
@@ -33,17 +34,77 @@ public class Struct implements NativeObject {
     }
 
     @Override
-    public MemoryAddress memoryAddress() {
-        return baseAddress;
+    public void close() {
+        segment.close();
     }
 
     @Override
-    public long sizeOf() {
+    public MemoryAddress baseAddress() {
+        return segment.baseAddress();
+    }
+
+    @Override
+    public Thread ownerThread() {
+        return segment.ownerThread();
+    }
+
+    @Override
+    public MemorySegment withOwnerThread(Thread newOwner) {
+        return segment.withOwnerThread(newOwner);
+    }
+
+    @Override
+    public long byteSize() {
         return segment.byteSize();
     }
 
     @Override
-    public void close() {
-        segment.close();
+    public MemorySegment withAccessModes(int accessModes) {
+        return segment.withAccessModes(accessModes);
+    }
+
+    @Override
+    public boolean hasAccessModes(int accessModes) {
+        return segment.hasAccessModes(accessModes);
+    }
+
+    @Override
+    public int accessModes() {
+        return segment.accessModes();
+    }
+
+    @Override
+    public MemorySegment asSlice(long offset, long newSize) {
+        return segment.asSlice(offset, newSize);
+    }
+
+    @Override
+    public boolean isAlive() {
+        return segment.isAlive();
+    }
+
+    @Override
+    public MemorySegment fill(byte value) {
+        return segment.fill(value);
+    }
+
+    @Override
+    public void copyFrom(MemorySegment src) {
+        segment.copyFrom(src);
+    }
+
+    @Override
+    public long mismatch(MemorySegment other) {
+        return segment.mismatch(other);
+    }
+
+    @Override
+    public ByteBuffer asByteBuffer() {
+        return segment.asByteBuffer();
+    }
+
+    @Override
+    public byte[] toByteArray() {
+        return segment.toByteArray();
     }
 }
