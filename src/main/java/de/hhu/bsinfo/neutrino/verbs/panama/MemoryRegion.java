b72c29de15746e9435547eebd89ad7e132684411
Filip Krakowski <krakowski@hhu.de>
2020-08-13 20:31:41 +0200

    Add MemoryRegion

---
+++ src/main/java/de/hhu/bsinfo/neutrino/verbs/panama/MemoryRegion.java
@@ -0,0 +1,73 @@
+package de.hhu.bsinfo.neutrino.verbs.panama;
+
+import static org.linux.rdma.ibverbs_h.*;
+
+import de.hhu.bsinfo.neutrino.verbs.panama.util.Struct;
+import jdk.incubator.foreign.MemoryAddress;
+
+public final class MemoryRegion extends Struct {
+
+    public MemoryRegion() {
+        super(ibv_mr.allocate());
+    }
+
+    public MemoryRegion(MemoryAddress address) {
+        super(address, ibv_mr.$LAYOUT());
+    }
+
+    public MemoryAddress getContext() {
+        return ibv_mr.context$get(segment());
+    }
+
+    public MemoryAddress getProtectionDomain() {
+        return ibv_mr.pd$get(segment());
+    }
+
+    public MemoryAddress getMemoryRegionAddress() {
+        return ibv_mr.addr$get(segment());
+    }
+
+    public long getLength() {
+        return ibv_mr.length$get(segment());
+    }
+
+    public int getHandle() {
+        return ibv_mr.handle$get(segment());
+    }
+
+    public int getLocalKey() {
+        return ibv_mr.lkey$get(segment());
+    }
+
+    public int getRemoteKey() {
+        return ibv_mr.rkey$get(segment());
+    }
+
+    public void setContext(final MemoryAddress value) {
+        ibv_mr.context$set(segment(), value);
+    }
+
+    public void setProtectionDomain(final MemoryAddress value) {
+        ibv_mr.pd$set(segment(), value);
+    }
+
+    public void setMemoryRegionAddress(final MemoryAddress value) {
+        ibv_mr.addr$set(segment(), value);
+    }
+
+    public void setLength(final long value) {
+        ibv_mr.length$set(segment(), value);
+    }
+
+    public void setHandle(final int value) {
+        ibv_mr.handle$set(segment(), value);
+    }
+
+    public void setLocalKey(final int value) {
+        ibv_mr.lkey$set(segment(), value);
+    }
+
+    public void setRemoteKey(final int value) {
+        ibv_mr.rkey$set(segment(), value);
+    }
+}
