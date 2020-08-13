ce43e1dac4cbc696067e7e4b36596694862e334d
Filip Krakowski <krakowski@hhu.de>
2020-08-13 22:07:32 +0200

    Add ScatterGatherElement

---
+++ src/main/java/de/hhu/bsinfo/neutrino/verbs/panama/ScatterGatherElement.java
@@ -0,0 +1,41 @@
+package de.hhu.bsinfo.neutrino.verbs.panama;
+
+import static org.linux.rdma.ibverbs_h.*;
+
+import de.hhu.bsinfo.neutrino.verbs.panama.util.Struct;
+import jdk.incubator.foreign.MemoryAddress;
+
+public final class ScatterGatherElement extends Struct {
+
+    public ScatterGatherElement() {
+        super(ibv_sge.allocate());
+    }
+
+    public ScatterGatherElement(MemoryAddress address) {
+        super(address, ibv_sge.$LAYOUT());
+    }
+
+    public long getMemoryAddress() {
+        return ibv_sge.addr$get(segment());
+    }
+
+    public int getLength() {
+        return ibv_sge.length$get(segment());
+    }
+
+    public int getLocalKey() {
+        return ibv_sge.lkey$get(segment());
+    }
+
+    public void setMemoryAddress(final long value) {
+        ibv_sge.addr$set(segment(), value);
+    }
+
+    public void setLength(final int value) {
+        ibv_sge.length$set(segment(), value);
+    }
+
+    public void setLocalKey(final int value) {
+        ibv_sge.lkey$set(segment(), value);
+    }
+}
