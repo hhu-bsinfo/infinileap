d679b851c747e7bd2e4d7dc031ae169bf49e7c42
Filip Krakowski <krakowski@hhu.de>
2023-02-23 21:14:50 +0100

    Implement epoll event loop test

---
+++ engine/src/test/java/de/hhu/bsinfo/infinileap/engine/event/loop/EpollEventLoopTest.java
@@ -0,0 +1,46 @@
+package de.hhu.bsinfo.infinileap.engine.event.loop;
+
+import de.hhu.bsinfo.infinileap.common.multiplex.EventFileDescriptor;
+import de.hhu.bsinfo.infinileap.common.multiplex.EventFileDescriptor.OpenMode;
+import de.hhu.bsinfo.infinileap.common.multiplex.EventType;
+import de.hhu.bsinfo.infinileap.common.multiplex.SelectionKey;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EpollEventLoopTest {
+
+    private static final Integer ATTACHEMENT = 42;
+
+    private static final int TIMEOUT = 5;
+
+    @Test
+    public void testEventFileDescriptor() throws Exception {
+
+        final var latch = new CountDownLatch(1);
+        var eventLoop = new EpollEventLoop<Integer>() {
+            @Override
+            protected void process(SelectionKey<Integer> selectionKey) throws IOException {
+                System.out.println("HELLO");
+                assertEquals(selectionKey.attachment(), ATTACHEMENT);
+                latch.countDown();
+            }
+        };
+
+        // Start the event loop
+        eventLoop.start(Thread::new);
+
+        // Create event file descriptor
+        var descriptor = EventFileDescriptor.create(OpenMode.NONBLOCK);
+        eventLoop.add(descriptor, ATTACHEMENT, EventType.EPOLLIN, EventType.EPOLLET);
+
+        // Write to event file descriptor and wait for loop to wake up
+        descriptor.fire();
+        assertTrue(latch.await(TIMEOUT, TimeUnit.SECONDS));
+    }
+}
