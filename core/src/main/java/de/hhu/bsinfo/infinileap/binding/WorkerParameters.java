ec6362c2748684998003ee10389df5a3dba28fff
Filip Krakowski <krakowski@hhu.de>
2023-02-23 22:07:38 +0100

    Add wake up events to worker parameters

--- core/src/main/java/de/hhu/bsinfo/infinileap/binding/WorkerParameters.java
+++ core/src/main/java/de/hhu/bsinfo/infinileap/binding/WorkerParameters.java
@@ -2,6 +2,7 @@ package de.hhu.bsinfo.infinileap.binding;
 
 import de.hhu.bsinfo.infinileap.common.util.NativeObject;
 import de.hhu.bsinfo.infinileap.common.util.BitMask;
+import de.hhu.bsinfo.infinileap.common.util.flag.IntegerFlag;
 import de.hhu.bsinfo.infinileap.common.util.flag.LongFlag;
 
 import java.lang.foreign.SegmentAllocator;
@@ -28,6 +29,12 @@ public class WorkerParameters extends NativeObject {
         return this;
     }
 
+    public WorkerParameters setWakeupEvents(WakeupEvent... events) {
+        ucp_worker_params_t.events$set(segment(), BitMask.intOf(events));
+        addFieldMask(Field.EVENTS);
+        return this;
+    }
+
     public WorkerParameters setClientId(long clientId) {
         ucp_worker_params_t.client_id$set(segment(), clientId);
         addFieldMask(Field.CLIENT_ID);
@@ -44,6 +51,27 @@ public class WorkerParameters extends NativeObject {
 
     private void addFieldMask(Field... fields) {
         ucp_worker_params_t.field_mask$set(segment(), BitMask.longOf(fields) | getFieldMask());
+    }
+
+    public enum WakeupEvent implements IntegerFlag {
+        REMOTE_MEMORY_ACCESS(UCP_WAKEUP_RMA()),
+        ATOMIC_OPERATION(UCP_WAKEUP_AMO()),
+        TAG_SEND(UCP_WAKEUP_TAG_SEND()),
+        TAG_RECEIVE(UCP_WAKEUP_TAG_RECV()),
+        TX(UCP_WAKEUP_TX()),
+        RX(UCP_WAKEUP_RX()),
+        EDGE(UCP_WAKEUP_EDGE());
+
+        private final int value;
+
+        WakeupEvent(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public int getValue() {
+            return value;
+        }
     }
 
     public enum Field implements LongFlag {
