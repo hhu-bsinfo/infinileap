1ee6c68c58857b68231ce3a207160386816082fe
Filip Krakowski <krakowski@hhu.de>
2023-02-23 21:54:52 +0100

    Implement epoll event loop test with UCP worker

--- engine/src/test/java/de/hhu/bsinfo/infinileap/engine/event/loop/EpollEventLoopTest.java
+++ engine/src/test/java/de/hhu/bsinfo/infinileap/engine/event/loop/EpollEventLoopTest.java
@@ -1,9 +1,11 @@
 package de.hhu.bsinfo.infinileap.engine.event.loop;
 
+import de.hhu.bsinfo.infinileap.binding.*;
 import de.hhu.bsinfo.infinileap.common.multiplex.EventFileDescriptor;
 import de.hhu.bsinfo.infinileap.common.multiplex.EventFileDescriptor.OpenMode;
 import de.hhu.bsinfo.infinileap.common.multiplex.EventType;
 import de.hhu.bsinfo.infinileap.common.multiplex.SelectionKey;
+import de.hhu.bsinfo.infinileap.util.ResourcePool;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -41,5 +43,43 @@ public class EpollEventLoopTest {
         // Write to event file descriptor and wait for loop to wake up
         descriptor.fire();
         assertTrue(latch.await(TIMEOUT, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void testUcpWorker() throws Exception {
+        var contextParameters = new ContextParameters()
+                .setFeatures(ContextParameters.Feature.WAKEUP);
+
+        var workerParameters = new WorkerParameters()
+                .setThreadMode(ThreadMode.SINGLE);
+
+        try (var pool = new ResourcePool()) {
+            var context = pool.push(Context.initialize(contextParameters));
+            var worker = pool.push(context.createWorker(workerParameters));
+
+            final var latch = new CountDownLatch(1);
+            var eventLoop = new EpollEventLoop<Integer>() {
+                @Override
+                protected void process(SelectionKey<Integer> selectionKey) throws IOException {
+                    assertEquals(selectionKey.attachment(), ATTACHEMENT);
+
+                    do {
+                        worker.progress();
+                    } while (worker.arm() != Status.OK);
+
+                    latch.countDown();
+                }
+            };
+
+            // Start the event loop
+            eventLoop.start(Thread::new);
+
+            // Create event file descriptor
+            eventLoop.add(worker, ATTACHEMENT, EventType.EPOLLIN);
+
+            // Write to event file descriptor and wait for loop to wake up
+            worker.signal();
+            assertTrue(latch.await(TIMEOUT, TimeUnit.SECONDS));
+        }
     }
 }
